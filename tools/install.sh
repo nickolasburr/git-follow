4c42dd7837e26dbc81b4099049e156899f8842fd
Nickolas Burr <nickolasburr@gmail.com>
2018-01-03 14:55:08 -0500

    Add core module installation/removal tasks

--- tools/install.sh
+++ tools/install.sh
@@ -10,17 +10,37 @@ if [[ ! -d "$PREFIX" ]]; then
 	exit 1
 fi
 
-BINDIR="$PREFIX/bin"
-
 TARGET="git-follow"
+MANDIR="man"
+SRCDIR="src"
+
+BINDIR="$PREFIX/bin"
+ETCDIR="$PREFIX/etc"
+MDLDIR="$ETCDIR/$TARGET"
+MDLSRC="$MDLDIR/$SRCDIR"
+
 MANPAGE="$TARGET.1.gz"
 MANDEST="$PREFIX/share/man/man1"
 
 INSTALL="/usr/bin/install"
 OPTIONS="-c"
 
+CP="/bin/cp"
+CPOPTS="-rf"
+
+SED="/usr/bin/sed"
+SEDOPTS="-i ''"
+SEDMATCH="s@$SRCDIR@$MDLSRC@g"
+
 cd ..
+cp "$MANDIR/$MANPAGE" "$MANDEST/$MANPAGE"
 
-cp "man/$MANPAGE" "$MANDEST/$MANPAGE"
-
+# Set absolute path for 'use lib' directive.
+eval "$SED $SEDOPTS $SEDMATCH $TARGET"
 eval "$INSTALL $OPTIONS $TARGET $BINDIR/$TARGET"
+
+if [[ ! -d "$MDLDIR" ]]; then
+	mkdir -p "$MDLDIR"
+fi
+
+eval "$CP $CPOPTS $SRCDIR $MDLSRC"
