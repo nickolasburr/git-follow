ad7f2df9fa07e5a050ad592a1bf98de5a55f1775
Nickolas Burr <nickolasburr@gmail.com>
2018-01-21 16:20:28 -0500

    Add more test conditions

--- tools/test.sh
+++ tools/test.sh
@@ -25,15 +25,18 @@ ERROR=0
 TESTS=(
 	"--last=5 --no-merges --no-renames"
 	"--first --no-patch --branch origin/master"
+	"--last --no-merges --no-patch"
+	"--func in_array --no-renames"
+	"--pickaxe git_track_map_aliases --last --no-patch"
 )
 
 for OPTIONS in "${TESTS[@]}"; do
 	eval "./$TARGET $OPTIONS -- $TARGET" >/dev/null 2>&1
 
 	if [[ $? -eq 0 ]]; then
-		printf 'OK: %s %s -- %s\n' "$TARGET" "$OPTIONS" "$TARGET"
+		printf 'OK: ./%s %s -- %s\n' "$TARGET" "$OPTIONS" "$TARGET"
 	else
-		printf 'ERROR: %s %s -- %s\n' "$TARGET" "$OPTIONS" "$TARGET"
+		printf 'ERROR: ./%s %s -- %s\n' "$TARGET" "$OPTIONS" "$TARGET"
 
 		ERROR=1
 
